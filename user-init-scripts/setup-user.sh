87c60ed3e295e4443bf4f9dced3e7f7e47aa3e4d
Jack Myers <jackhmyers97@gmail.com>
2024-08-10 22:53:15 -0700

    minor fix

--- user-init-scripts/setup-user.sh
+++ user-init-scripts/setup-user.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-set -x
-set -e
 
-export PATH="/usr/local/pixibin:$PATH"
 
 if [ ! -d ~/.local/share/chezmoi ]; then
+  set -x
+  set -e
+
   curl -fsSL https://pixi.sh/install.sh | bash
   export PATH=~/.pixi/bin:"$PATH"
   pixi global install git
@@ -12,7 +12,4 @@ if [ ! -d ~/.local/share/chezmoi ]; then
   pixi run --manifest-path ~/.local/share/chezmoi/pixi.toml install-user-tools 
 
   /etc/user-init-scripts/bin/make-user-exe-global
-fi
-
-if [ ! -d /usr/local/pixibin ]; then
 fi
